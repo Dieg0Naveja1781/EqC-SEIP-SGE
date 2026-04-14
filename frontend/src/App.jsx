ee661bad7e53fe4af690f73e6f8fd358825473cc
Roberto Perez Zamora <roberto.perez9296@alumnos.udg.mx>
2026-04-14 08:51:20 -0600

    (Fix) Cambios en app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { UserData } from "./modules/user-data/UserData"; // Importar módulo de datos de usuario
+import { ArchiveList } from "./modules/archives_list/archive_list";
 import { MainPage } from './modules/main'; // Importar módulo de la página principal
+
 function App() {
   return (
     <BrowserRouter>
@@ -9,10 +11,13 @@ function App() {
         <Link to="/" style={{ color: "white", marginRight: "10px" }}>
           Inicio
         </Link>
-        <Link to="/main" style={{ color: 'white', marginRight: '10px' }}>Main Page</Link>
-        <Link to="/perfil" style={{ color: "white" }}>
+        <Link to="/perfil" style={{ color: "white", marginRight: "10px" }}>
           Ver Perfil de Usuario
         </Link>
+        <Link to="/archive_list" style={{ color: "white" }}>
+          Gestión de Archivos
+        </Link>
+        <Link to="/main" style={{ color: 'white', marginRight: '10px' }}>Main Page</Link>
       </nav>
 
       <Routes>
@@ -22,6 +27,7 @@ function App() {
         />
         <Route path="/main" element={<MainPage />} />
         <Route path="/perfil" element={<UserData />} />
+        <Route path="/archive_list" element={<ArchiveList />} />
       </Routes>
     </BrowserRouter>
   );
