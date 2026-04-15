f5eaa3f8e38395077d012a54ce130b6beff226d6
andrei13 <diego.jimenez5837@alumnos.udg.mx>
2026-04-15 00:43:45 -0600

    docs:se actualizo la arquitectura de los archivos para modular

--- gestion_expedientes/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GestionExpedientesConfig(AppConfig):
-    name = 'gestion_expedientes'
--- gestion_expedientes/views.py
+++
@@ -1,4 +0,0 @@
-#Aquí se escribira la lógica de qué pasa cuando alguien sube un archivo o busca un expediente
-from django.shortcuts import render
-
-# Create your views here.
