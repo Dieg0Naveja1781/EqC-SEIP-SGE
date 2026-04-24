ab006d6b2a1b657c90d7a54f1fd841381d356463
Diego A. Naveja L. <diego.naveja4913@alumnos.udg.mx>
2026-04-23 18:48:26 -0600

    (build) -> Actualizo el archivo App para definir las rutas de la web

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,20 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { UserData } from "./modules/user-data/UserData"; // Importar módulo de datos de usuario
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Importación de Módulos (Cada uno en su carpeta correspondiente)
+import { LandingPage } from "./modules/landing-page/LandingPage"; 
+import { MainPage } from "./modules/main";
+import { UserData } from "./modules/user-data/UserData";
 import { ArchiveList } from "./modules/archives_list/archive_list";
-import { MainPage } from './modules/main'; // Importar módulo de la página principal
 
 function App() {
   return (
     <BrowserRouter>
-      {/* Esto es un menú de navegación simple para probar */}
-      <nav style={{ padding: "20px", backgroundColor: "#333", color: "white" }}>
-        <Link to="/" style={{ color: "white", marginRight: "10px" }}>
-          Inicio
-        </Link>
-        <Link to="/main" style={{ color: 'white', marginRight: '10px' }}>Main Page
-        </Link>
-        <Link to="/perfil" style={{ color: "white", marginRight: "10px" }}>
-          Ver Perfil de Usuario
-        </Link>
-        <Link to="/archive_list" style={{ color: "white" }}>
-          Gestión de Archivos
-        </Link>
-      </nav>
-
       <Routes>
-        <Route
-          path="/"
-          element={<h1>Bienvenido al Sistema de Expedientes</h1>}
-        />
+        {/* RUTA RAÍZ: Muestra la Landing Page */}
+        <Route path="/" element={<LandingPage />} />
+
+        {/* RUTAS INTERNAS DEL SISTEMA */}
+        {/* Aquí deben agregar las rutas para cada uno de los módulos */}
         <Route path="/main" element={<MainPage />} />
         <Route path="/perfil" element={<UserData />} />
         <Route path="/archive_list" element={<ArchiveList />} />
